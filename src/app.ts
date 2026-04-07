2156e442fc7d43282fadf1b9110d9a17ee0b8e81
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-04-07 20:31:52 +0300

    changed route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userModel from './models/user.model';
+import userRouter from "./routes/user.routes";
+ 
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.get('/api/users', (req, res) => {
   
