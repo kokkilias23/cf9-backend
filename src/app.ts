2bc267364f8610e1a21bdd1ea162396005964e7f
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-04-20 19:26:58 +0300

    app.ts recovered

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,7 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
-<<<<<<< HEAD
-import userRoutes from './routes/user.routes';  
-dotenv.config();
 
-const app = express();
-app.use(express.json());
-
-=======
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
 
@@ -18,7 +11,6 @@ const app = express();
 app.use(express.json())
 
 app.use('/api/auth', authRoutes);
->>>>>>> 8774fe84a0c7f097bfa0ccc50c963b6957b6c2eb
 app.use('/api/users', userRoutes);
 
 
